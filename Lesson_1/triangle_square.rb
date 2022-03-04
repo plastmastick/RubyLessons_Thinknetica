87571469b413e04cd676fb8ba46eaa4f750333bf
plastmastick <nick.razum23@gmail.com>
2022-03-04 10:25:30 +0300

    Add triangle_square.rb

---
+++ Lesson_1/triangle_square.rb
@@ -0,0 +1,10 @@
+puts "Программа вычисления площади треугольника."
+
+puts "Укажите значение для основания треугольника:"
+triangle_base = gets.chomp.to_i
+
+puts "Укажите значение для высоты треугольника:"
+triangle_height = gets.chomp.to_i
+
+triangle_square = 1 / 2 * triangle_base * triangle_height
+puts "Площадь заданного треугольника: #{triangle_square}"
