4a6103a04b7226ab53cc1d158d03e7b7c58ba075
plastmastick <nick.razum23@gmail.com>
2022-04-09 23:10:35 +0300

    fix task 10

--- Lesson_4/validation.rb
+++ Lesson_4/validation.rb
@@ -7,10 +7,10 @@ module Validation
   end
 
   module ClassMethods
-    def validate(attr = nil, option = nil, param = nil)
-      @validates ||= []
-      return @validates if attr.nil? || option.nil?
+    attr_reader :validates
 
+    def validate(attr, option, param = nil)
+      @validates ||= []
       @validates << [attr, option, param]
     end
   end
@@ -26,26 +26,29 @@ module Validation
     protected
 
     def validate!
-      self.class.validate.each do |validate|
-        attr = send validate[0]
-        option = validate[1]
-        param = validate[2]
-
-        case option
-        when :presence
-          raise "Attribute is nil! Attr: #{validate[0]}" if attr.nil?
-        when :format
-          raise "Attribute have invalid format! Attr: #{validate[0]}" unless attr =~ param
-        when :type
-          raise "Attribute have invalid type! Attr: #{validate[0]}" unless attr.is_a? param
-        when :comparison_min
-          raise "Value (#{attr}) less then #{param}. Attr: #{validate[0]}" if attr < param
-        when :comparison_min_length
-          raise "Value (#{attr}) less then #{param}. Attr: #{validate[0]}" if attr.length < param
-        else
-          raise "Unknown validation option! (#{option})"
-        end
+      self.class.validates.each do |attr, option, param|
+        send option, send(attr), param
       end
+    end
+
+    def presence(attr, _param)
+      raise "Attribute is nil! Attr: #{attr}" if attr.nil?
+    end
+
+    def format(attr, param)
+      raise "Attribute have invalid format! Attr: #{attr}" unless attr =~ param
+    end
+
+    def type(attr, param)
+      raise "Attribute have invalid type! Attr: #{attr}" unless attr.is_a? param
+    end
+
+    def comparison_min(attr, param)
+      raise "Value (#{attr}) less then #{param}!" if attr < param
+    end
+
+    def comparison_min_length
+      raise "Value (#{attr}) less then #{param}!" if attr.length < param
     end
   end
 end
